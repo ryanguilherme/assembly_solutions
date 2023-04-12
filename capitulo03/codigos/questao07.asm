097e30ee729ecbb646b879fbbc35a8facefb948f
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-12 14:43:40 -0300

    alteracoes q07cap03

---
+++ capitulo03/codigos/questao07.asm
@@ -0,0 +1,32 @@
+            .686
+            .model flat, c
+            .stack 100h
+printf      PROTO arg1:Ptr Byte, printlist:VARARG
+scanf       PROTO arg2:Ptr Byte, inputlist:VARARG
+
+            .data
+fah         sdword ?
+cel         sdword ?
+inpfmt      byte "%d", 0
+msg0fmt     byte "%s", 0
+msg1fmt     byte 0Ah, "%s%d", 0
+msg0        byte "Enter the degrees in Fahrenheit: ", 0
+msg1        byte "The degrees in Celsius is: ", 0
+
+            .code
+main        proc
+            INVOKE      printf, ADDR msg0fmt, ADDR msg0
+            INVOKE      scanf, ADDR inpfmt, ADDR fah
+            sub         fah, 32 ;guarda (F-32) em fah (fahrenheit)
+            mov         eax, fah ;move fah (F-32) para eax para dividi-lo depois
+            mov         ebx, 9 ; guarda 9 em ebx para dividir eax por ele
+            idiv        ebx ;divide eax por ebx (ebx/9)
+            mov         cel, eax
+            mov         ebx, 5 ;move 5 para ebx para multiplicar eax por ele
+            imul        ebx ;multiplica por 5 e encerra o calculo aqui
+            mov         cel, eax ;guarda o resultado final em cel (celsius)
+            INVOKE      printf, ADDR msg1fmt, ADDR msg1, cel
+            ret
+main        endp
+            end
+
---
+++ capitulo03/questao07.md
@@ -0,0 +1,13 @@
+# _7. Write a complete assembly language program to prompt for and input the temperature in degrees Fahrenheit, calculate the degrees in Celsius, and then output the degrees in Celsius. The equation to be used is C = ( F −32)/9 * 5, where C stands for Celsius and F stands for Fahrenheit. Note that the answer will be off slightly due to using integers and be very careful to use the proper order of operations. The form of the input and output can be found below. Be sure to use proper vertical and horizontal spacings:_
+### OBS: Comentários sobre o código no arquivo .asm referente ao código da questão
+<hr>
+
+## _Input and Output_
+<code style='color:pink'>
+Enter the degrees in Fahrenheit: 100
+The degrees in Celsius is: 35   
+</code>
+
+<hr>
+
+### **Resposta:**
