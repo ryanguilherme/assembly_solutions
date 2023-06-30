4a641ba474a9805389d4b3ed58c0adeb0630cd9a
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-06-29 21:36:26 -0300

    questao04 capitulo08 concluida

--- capitulo08/codigos/testes.asm
+++ capitulo08/codigos/testes.asm
@@ -6,20 +6,18 @@ scanf       PROTO arg2:Ptr Byte, inputlist:VARARG
 
             .data
 format      byte "%d", 0
-
-z           sdword 1, 2, 3, 4, 5
-i           sdword 2
-j           sdword 3
+narray      sdword 1, 2, 3, 4, 5
+marray      sdword 10 dup(?)
+oarray      sword 15, 20, 25
 
             .code
 main        PROC
-            ; z[i] = z[j]
-            mov         edx, j
-            mov         eax, z[edx*4]
-            mov         edx, i
-            mov         z[edx*4], eax
-
-            mov         eax, z+8
+        ;    mov         eax, lengthof narray
+        ;    mov         eax, sizeof   narray
+        ;    mov         eax, lengthof marray
+        ;    mov         eax, sizeof   marray
+        ;    mov         eax, lengthof oarray
+            mov         eax, sizeof   oarray
 
             INVOKE      printf, ADDR format, eax
             ret
