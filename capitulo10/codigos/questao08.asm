b7b1b28d210255f82aee1d0af63118ea2f08dba6
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-07-13 14:51:18 -0300

    questao 08 capitulo 10

--- capitulo10/codigos/questao08.asm
+++ capitulo10/codigos/questao08.asm
@@ -1,13 +1,54 @@
-            .686
-            .model flat, c
-            .stack 100h
-printf      PROTO   arg1:Ptr Byte, printlist:VARARG
-scanf       PROTO   arg2:Ptr Byte, inputlist:VARARG
-            .data
+            .686 
+            .model flat,c 
+            .stack 100h 
+printf      PROTO arg1:Ptr Byte, printlist:VARARG 
+scanf       PROTO arg2:Ptr Byte, inputlist:VARARG 
 
-            .code
-main        proc
+            .data 
+in1fmt      byte "%lf",0 
+in2fmt      byte "%d",0
+msg0fmt     byte 0Ah,"%s",0 
+msg1fmt     byte 0Ah,"%s%6.1f",0Ah,0 
+msg0        byte "Enter a double for x: ",0 
+msg1        byte "The sum is: ",0 
+msg2        byte "enter the number of elements for the sum: ",0
+
+x           real8 ? 
+sum         real8 ? 
+zero        real8 0.0 
+value       sdword ?
+cont        sdword 0
+
+            .code 
+main        proc 
+            ;sum = 0.0 
+            fld zero 
+            fstp sum 
+            INVOKE printf, ADDR msg0fmt, ADDR msg2 
+            INVOKE scanf, ADDR in2fmt, ADDR value 
+            mov eax, value
             
-            ret
-main        endp
-            end
+while01:    nop ;while cont < value 
+        
+            push eax
+            INVOKE printf, ADDR msg0fmt, ADDR msg0 
+            INVOKE scanf, ADDR in1fmt, ADDR x 
+
+            ;sum += x
+            fld sum 
+            fadd x 
+            fstp sum 
+        
+            inc cont
+            pop eax
+            
+            cmp cont, eax ;if cont < value goto while01
+            jg endw01
+            jmp while01
+endw01:     nop 
+
+            INVOKE printf, ADDR msg1fmt, ADDR msg1, sum 
+
+            ret 
+main        endp 
+            end 
--- capitulo10/questao08.md
+++ capitulo10/questao08.md
@@ -1,3 +1,59 @@
 # **8.** _Change the complete program in Sect. 10.9 to implement a do-while structuren (post-test loop) instead of while structure (pre-test loop). Make sure that it works properly for 0.0 and negative numbers._
 [Código ASM aqui](codigos/questao08.asm)
 
+```asm
+            .686 
+            .model flat,c 
+            .stack 100h 
+printf      PROTO arg1:Ptr Byte, printlist:VARARG 
+scanf       PROTO arg2:Ptr Byte, inputlist:VARARG 
+
+            .data 
+in1fmt      byte "%lf",0 
+in2fmt      byte "%d",0
+msg0fmt     byte 0Ah,"%s",0 
+msg1fmt     byte 0Ah,"%s%6.1f",0Ah,0 
+msg0        byte "Enter a double for x: ",0 
+msg1        byte "The sum is: ",0 
+msg2        byte "enter the number of elements for the sum: ",0
+
+x           real8 ? 
+sum         real8 ? 
+zero        real8 0.0 
+value       sdword ?
+cont        sdword 0
+
+            .code 
+main        proc 
+            ;sum = 0.0 
+            fld zero 
+            fstp sum 
+            INVOKE printf, ADDR msg0fmt, ADDR msg2 
+            INVOKE scanf, ADDR in2fmt, ADDR value 
+            mov eax, value
+            
+while01:    nop ;while cont < value 
+        
+            push eax
+            INVOKE printf, ADDR msg0fmt, ADDR msg0 
+            INVOKE scanf, ADDR in1fmt, ADDR x 
+
+            ;sum += x
+            fld sum 
+            fadd x 
+            fstp sum 
+        
+            inc cont
+            pop eax
+            
+            cmp cont, eax ;if cont < value goto while01
+            jg endw01
+            jmp while01
+endw01:     nop 
+
+            INVOKE printf, ADDR msg1fmt, ADDR msg1, sum 
+
+            ret 
+main        endp 
+            end 
+```
